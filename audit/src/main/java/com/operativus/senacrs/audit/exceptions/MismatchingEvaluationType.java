8c025df54ec5e080ed2a728686a41b93e2c27666
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-25 13:36:11 -0200

    Suppressing serial warning
    OPEN - task 6: Data model for form 
    http://github.com/fgka/senac-auditing/issues/issue/6

--- audit/src/main/java/com/operativus/senacrs/audit/exceptions/MismatchingEvaluationType.java
+++ audit/src/main/java/com/operativus/senacrs/audit/exceptions/MismatchingEvaluationType.java
@@ -1,6 +1,6 @@
 package com.operativus.senacrs.audit.exceptions;
 
-import com.operativus.senacrs.audit.model.EvaluationActivity;
+import com.operativus.senacrs.audit.model.AbstractSequenceStringFieldComparable;
 import com.operativus.senacrs.audit.model.EvaluationGrade;
 
 
@@ -8,7 +8,7 @@ import com.operativus.senacrs.audit.model.EvaluationGrade;
 public class MismatchingEvaluationType extends IllegalArgumentException {
 	
 	
-	public MismatchingEvaluationType(EvaluationActivity activity, EvaluationGrade grade) {
+	public MismatchingEvaluationType(AbstractSequenceStringFieldComparable activity, EvaluationGrade grade) {
 		
 		// TODO Auto-generated constructor stub
 	}
