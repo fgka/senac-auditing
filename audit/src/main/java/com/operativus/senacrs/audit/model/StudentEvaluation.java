ba116772ccc40bf36604cb30744f00dfc5228458
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-25 12:55:21 -0200

    Students have an order to be listed.
    OPEN - task 6: Data model for form 
    http://github.com/fgka/senac-auditing/issues/issue/6

--- audit/src/main/java/com/operativus/senacrs/audit/model/StudentEvaluation.java
+++ audit/src/main/java/com/operativus/senacrs/audit/model/StudentEvaluation.java
@@ -5,46 +5,58 @@ import java.util.Map;
 
 import com.operativus.senacrs.audit.exceptions.MismatchingEvaluationType;
 
+public class StudentEvaluation implements Comparable<StudentEvaluation>{
 
-public class StudentEvaluation {
-
+	private final int sequence;
 	private final String name;
 	private Map<EvaluationActivity, EvaluationGrade> grades = new HashMap<EvaluationActivity, EvaluationGrade>();
 	private EvaluationGrade finalGrade = null;
-	public StudentEvaluation(String name) {
+
+	public StudentEvaluation(int sequence, String name) {
 
 		super();
+		this.sequence = sequence;
 		this.name = name;
 	}
-	
+
+	public String getName() {
+
+		return name;
+	}
+
+	public int getSequence() {
+
+		return sequence;
+	}
+
 	public Map<EvaluationActivity, EvaluationGrade> getGrades() {
-	
+
 		return grades;
 	}
-	
+
 	public EvaluationGrade putGrade(EvaluationActivity activity, EvaluationGrade grade) {
-	
+
 		if (activity.getType() != grade.getType()) {
 			throw new MismatchingEvaluationType(activity, grade);
 		}
-		
+
 		return this.grades.put(activity, grade);
 	}
-	
+
 	public EvaluationGrade getFinalGrade() {
-	
+
 		return finalGrade;
 	}
-	
+
 	public void setFinalGrade(EvaluationGrade finalGrade) {
-	
+
 		this.finalGrade = finalGrade;
 	}
-	
-	public String getName() {
-	
-		return name;
+
+	@Override
+	public int compareTo(StudentEvaluation o) {
+
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	
-	
 }
