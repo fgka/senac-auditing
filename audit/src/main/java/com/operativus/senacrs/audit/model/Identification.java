a843cc66ad7677b1f205246370b259afc117958e
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-25 12:54:54 -0200

    Added last day field
    OPEN - task 6: Data model for form 
    http://github.com/fgka/senac-auditing/issues/issue/6

--- audit/src/main/java/com/operativus/senacrs/audit/model/Identification.java
+++ audit/src/main/java/com/operativus/senacrs/audit/model/Identification.java
@@ -1,5 +1,7 @@
 package com.operativus.senacrs.audit.model;
 
+import java.util.Date;
+
 public class Identification {
 
 	private String course = null;
@@ -7,6 +9,7 @@ public class Identification {
 	private String academic = null;
 	private String classDesc = null;
 	private String semester = null;
+	private Date lastDay = null;
 
 	public Identification(final String course, final String unit, final String academic,
 			final String classDesc, final String semester) {
@@ -20,63 +23,64 @@ public class Identification {
 		this.semester = semester;
 	}
 
-	
 	public String getCourse() {
-	
+
 		return course;
 	}
 
-	
 	public void setCourse(String course) {
-	
+
 		this.course = course;
 	}
 
-	
 	public String getUnit() {
-	
+
 		return unit;
 	}
 
-	
 	public void setUnit(String unit) {
-	
+
 		this.unit = unit;
 	}
 
-	
 	public String getAcademic() {
-	
+
 		return academic;
 	}
 
-	
 	public void setAcademic(String academic) {
-	
+
 		this.academic = academic;
 	}
 
-	
 	public String getClassDesc() {
-	
+
 		return classDesc;
 	}
 
-	
 	public void setClassDesc(String classDesc) {
-	
+
 		this.classDesc = classDesc;
 	}
 
-	
 	public String getSemester() {
-	
+
 		return semester;
 	}
 
-	
 	public void setSemester(String semester) {
-	
+
 		this.semester = semester;
 	}
+
+	public Date getLastDay() {
+
+		return lastDay;
+	}
+
+	public void setLastDay(Date lastDay) {
+
+		this.lastDay = lastDay;
+	}
+
 }
