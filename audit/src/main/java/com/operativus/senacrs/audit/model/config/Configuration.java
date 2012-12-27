85ab91b125c0a013d72207756276f63a40832152
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-27 20:02:40 -0200

    Not passing tests yet
    OPEN - task 8: Configuration file reader 
    http://github.com/fgka/senac-auditing/issues/issue/8

--- audit/src/main/java/com/operativus/senacrs/audit/model/config/Configuration.java
+++ audit/src/main/java/com/operativus/senacrs/audit/model/config/Configuration.java
@@ -60,6 +60,52 @@ public class Configuration {
 	
 		this.password = password;
 	}
-	
-	
+
+
+	@Override
+	public int hashCode() {
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((baseUrl == null) ? 0 : baseUrl.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Configuration other = (Configuration) obj;
+		if (baseUrl == null) {
+			if (other.baseUrl != null)
+				return false;
+		} else if (!baseUrl.equals(other.baseUrl))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+		
 }
--- audit/src/main/java/com/operativus/senacrs/audit/model/config/ConfigurationFactory.java
+++ audit/src/main/java/com/operativus/senacrs/audit/model/config/ConfigurationFactory.java
@@ -5,32 +5,46 @@ import java.io.File;
 
 public final class ConfigurationFactory {
 
-	private static final ConfigurationFactory instance = new ConfigurationFactory();
 	private static final String PWD = System.getProperty("user.dir");
 	private static final String DEFAULT_CONFIGURATION_FILENAME = PWD
 			+ File.separator + "configuration.properties";
+	protected static enum ConfigKey {
+		
+		BASE_URL("base_url"),
+		VERSION("version"),
+		USERNAME("username"),
+		PASSWORD("password"),
+		;
+		
+		private String key;
+		
+		private ConfigKey(String key) {
+			
+			this.key = key;			
+		}
+				
+		public String getKey() {
+
+			return key;
+		}
+	}
 		
 	private ConfigurationFactory() {
 
 		super();
 	}
 
-	public Configuration createConfiguration() {
+	public static Configuration createConfiguration() {
 		
 		return createConfiguration(DEFAULT_CONFIGURATION_FILENAME);
 	}
 
-	public Configuration createConfiguration(String filename) {
+	public static Configuration createConfiguration(String filename) {
 		
 		Configuration result = null;
 		
 		//TODO
 		
 		return result;
-	}
-		
-	public static ConfigurationFactory getInstance() {
-
-		return instance;
 	}
 }
