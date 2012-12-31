4d0e97dd52b65efbc6dc6067c33a2fa315335977
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-31 17:29:26 -0200

    Removed test unnecessary setup/tear down
    OPEN - task 14: Create node structure with "visitors" 
    http://github.com/fgka/senac-auditing/issues/issue/14

--- audit/src/test/java/com/operativus/senacrs/audit/graph/nodes/NodeTest.java
+++ audit/src/test/java/com/operativus/senacrs/audit/graph/nodes/NodeTest.java
@@ -1,22 +1,10 @@
 package com.operativus.senacrs.audit.graph.nodes;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 
 public class NodeTest {
-
-	@Before
-	public void setUp() throws Exception {
-
-	}
-
-	@After
-	public void tearDown() throws Exception {
-
-	}
 
 	@Test
 	public void testStart() {
