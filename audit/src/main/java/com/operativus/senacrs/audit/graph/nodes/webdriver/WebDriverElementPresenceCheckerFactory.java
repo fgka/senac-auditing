b948c87aab4e9a63570d484d7de31c59041de3fd
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-31 19:56:36 -0200

    just checking type validity and switching between types.
    OPEN - task 14: Create node structure with "visitors" 
    http://github.com/fgka/senac-auditing/issues/issue/14

--- audit/src/main/java/com/operativus/senacrs/audit/graph/nodes/webdriver/WebDriverElementPresenceCheckerFactory.java
+++ audit/src/main/java/com/operativus/senacrs/audit/graph/nodes/webdriver/WebDriverElementPresenceCheckerFactory.java
@@ -1,5 +1,7 @@
 package com.operativus.senacrs.audit.graph.nodes.webdriver;
 
+import com.operativus.senacrs.audit.exceptions.RuntimeExceptionFactory;
+
 
 public final class WebDriverElementPresenceCheckerFactory {
 
@@ -10,6 +12,31 @@ public final class WebDriverElementPresenceCheckerFactory {
 	
 	public static WebDriverElementPresenceChecker createChecker(WebDriverCheckerTypeEnum type) {
 		
+		if (type == null) {
+			throw RuntimeExceptionFactory.getInstance().getNullArgumentException("type");
+		}
+		
+		return internCreateChecker(type);
+	}
+
+	private static WebDriverElementPresenceChecker internCreateChecker(WebDriverCheckerTypeEnum type) {
+		
+		WebDriverElementPresenceChecker result = null;
+
+		switch (type) {
+		case DASHBOARD:
+			result = createDashboardChecker();
+			break;
+		default:
+			throw new IllegalArgumentException(type.name());
+		} 
+				
+		return result;
+	}
+
+	protected static WebDriverElementPresenceChecker createDashboardChecker() {
+
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
