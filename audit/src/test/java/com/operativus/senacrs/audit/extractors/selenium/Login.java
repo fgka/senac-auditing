75b6edc519b2a25b4da101d736b80a58bd8e0461
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-31 20:54:47 -0200

    Ignoring selenium generated tests

--- audit/src/test/java/com/operativus/senacrs/audit/extractors/selenium/Login.java
+++ audit/src/test/java/com/operativus/senacrs/audit/extractors/selenium/Login.java
@@ -7,12 +7,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+@Ignore
 public class Login {
 
 	private WebDriver driver;
