7f025af9530b4dd2d1cc7e90de36033b69afda20
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-31 19:08:45 -0200

    Behaviour captured.
    OPEN - task 14: Create node structure with "visitors" 
    http://github.com/fgka/senac-auditing/issues/issue/14

--- audit/src/test/java/com/operativus/senacrs/audit/graph/nodes/webdriver/DefaultWebDriverNodeTest.java
+++ audit/src/test/java/com/operativus/senacrs/audit/graph/nodes/webdriver/DefaultWebDriverNodeTest.java
@@ -1,34 +1,53 @@
 package com.operativus.senacrs.audit.graph.nodes.webdriver;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.mockito.Mockito;
+import org.openqa.selenium.WebDriver;
 
 public class DefaultWebDriverNodeTest {
 
+	WebDriver driver = null;
+	WebDriverElementPresenceChecker checker = null;
+	
+	
 	@Before
 	public void setUp() throws Exception {
 
+		driver = Mockito.mock(WebDriver.class);
+		checker = Mockito.mock(WebDriverElementPresenceChecker.class);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+
+		driver = null;
+		checker = null;
+	}
+
+	@Test
+	public void testDefaultWebDriverNodeNull() {
+
+		try {
+			new DefaultWebDriverNode(null);
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
 
 	}
 
 	@Test
 	public void testVerifyStateConditions() {
 
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDefaultWebDriverNode() {
-
-		fail("Not yet implemented");
+		DefaultWebDriverNode obj = null;
+		
+		obj = new DefaultWebDriverNode(checker);
+		obj.verifyStateConditions(driver);
+		
+		Mockito.verify(checker).hasAll(driver);
 	}
 
 }
