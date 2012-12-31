dbbba6a89cd00e4b089e33ea8fc09f7966518de0
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2012-12-31 18:19:13 -0200

    A WebDriver node should only check if the current driver is at the correct state.
    OPEN - task 14: Create node structure with "visitors" 
    http://github.com/fgka/senac-auditing/issues/issue/14

---
+++ audit/src/main/java/com/operativus/senacrs/audit/graph/nodes/webdriver/WebDriverNode.java
@@ -0,0 +1,11 @@
+package com.operativus.senacrs.audit.graph.nodes.webdriver;
+
+import org.openqa.selenium.WebDriver;
+
+import com.operativus.senacrs.audit.graph.nodes.Node;
+
+
+public interface WebDriverNode extends Node {	
+	
+	boolean verifyState(WebDriver driver);
+}
