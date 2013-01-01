f933012e12eb57e5e62087ccecd884ef245d099f
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-01 15:39:50 -0200

    Added known prefixes.
    OPEN - task 4: Put all XPath on an external file 
    http://github.com/fgka/senac-auditing/issues/issue/4

--- audit/src/main/java/com/operativus/senacrs/audit/properties/xpath/XPathPrefixesEnum.java
+++ audit/src/main/java/com/operativus/senacrs/audit/properties/xpath/XPathPrefixesEnum.java
@@ -2,7 +2,14 @@ package com.operativus.senacrs.audit.properties.xpath;
 
 public enum XPathPrefixesEnum {
 
-	DASHBOARD("xpath.dashboard"), ;
+	DASHBOARD("xpath.dashboard"),
+	ABOUT("xpath.about"),
+	LOGIN("xpath.login"),
+	PORTAL("xpath.portal"),
+	CLASS("xpath.class"),
+	GRADES("xpath.grades"),
+	PLAN("xpath.plan"),
+	;
 
 	private final String keyPrefix;
 
