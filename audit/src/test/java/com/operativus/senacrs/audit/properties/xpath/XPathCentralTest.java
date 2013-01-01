169b30d15d67682ae1a0290eeaa21e4387d93a2e
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-01 15:34:56 -0200

    Basic copy from MessagesCentral modified for reading xpath.properties
    OPEN - task 4: Put all XPath on an external file 
    http://github.com/fgka/senac-auditing/issues/issue/4

---
+++ audit/src/test/java/com/operativus/senacrs/audit/properties/xpath/XPathCentralTest.java
@@ -0,0 +1,44 @@
+package com.operativus.senacrs.audit.properties.xpath;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.operativus.senacrs.audit.properties.PropertyKey;
+
+public class XPathCentralTest {
+
+	private static enum TestMessagesEnum implements PropertyKey {
+
+		TEST_XPATH_PROPERTIES("test.xpath.properties"), ;
+
+		private final String key;
+
+		private TestMessagesEnum(final String key) {
+
+			this.key = key;
+		}
+
+		@Override
+		public String getKey() {
+
+			return this.key;
+		}
+
+	}
+
+	private static final String TEST_XPATH = "//xpath/test";
+	private static final PropertyKey[] PRE_LOADED_KEYS = new PropertyKey[] {
+			TestMessagesEnum.TEST_XPATH_PROPERTIES,
+	};
+
+	@Test
+	public void testGetMessagePreLoaded() {
+
+		String result = null;
+
+		for (PropertyKey k : PRE_LOADED_KEYS) {
+			result = XPathCentral.getXPath(k);
+			Assert.assertEquals(TEST_XPATH, result);
+		}
+	}
+}
