13361b74516f46390d7570e747eb9fc7d1052d2d
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-01 15:54:50 -0200

    Behaviour captured
    OPEN - task 4: Put all XPath on an external file 
    http://github.com/fgka/senac-auditing/issues/issue/4

--- audit/src/test/java/com/operativus/senacrs/audit/properties/xpath/XPathCentralTest.java
+++ audit/src/test/java/com/operativus/senacrs/audit/properties/xpath/XPathCentralTest.java
@@ -7,13 +7,13 @@ import com.operativus.senacrs.audit.properties.PropertyKey;
 
 public class XPathCentralTest {
 
-	private static enum TestMessagesEnum implements PropertyKey {
+	private static enum TestXPathEnum implements PropertyKey {
 
 		TEST_XPATH_PROPERTIES("test.xpath.properties"), ;
 
 		private final String key;
 
-		private TestMessagesEnum(final String key) {
+		private TestXPathEnum(final String key) {
 
 			this.key = key;
 		}
@@ -26,9 +26,33 @@ public class XPathCentralTest {
 
 	}
 
-	private static final String TEST_XPATH = "//xpath/test";
+	private static enum TestXPathPrefixEnum implements XPathKeyPrefix {
+
+		XPATH_TEST("xpath.test"),
+		NONE("none.none"),
+		;
+
+		private final String keyPrefix;
+
+		private TestXPathPrefixEnum(final String keyPrefix) {
+
+			this.keyPrefix = keyPrefix;
+		}
+
+		@Override
+		public String getKeyPrefix() {
+
+			return this.keyPrefix;
+		}
+	}
+
+	private static final String XPATH_TEST = "//xpath/test";
 	private static final PropertyKey[] PRE_LOADED_KEYS = new PropertyKey[] {
-			TestMessagesEnum.TEST_XPATH_PROPERTIES,
+			TestXPathEnum.TEST_XPATH_PROPERTIES,
+	};
+	private static final String[] XPATH_TEST_PREFIX_CONTENT = new String[] {
+		"//xpath/path1",
+		"//xpath/path2",
 	};
 
 	@Test
@@ -38,12 +62,38 @@ public class XPathCentralTest {
 
 		for (PropertyKey k : PRE_LOADED_KEYS) {
 			result = XPathCentral.getXPath(k);
-			Assert.assertEquals(TEST_XPATH, result);
+			Assert.assertEquals(XPATH_TEST, result);
 		}
 	}
+
+	@Test
+	public void testGetXPathByPrefixNull() throws Exception {
+
+		try {
+			XPathCentral.getXPathByPrefix(null);
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+	}
+
+	@Test
+	public void testGetXPathByPrefixInexistent() throws Exception {
+
+		String[] result = null;
+
+		result = XPathCentral.getXPathByPrefix(TestXPathPrefixEnum.NONE);		
+		Assert.assertNotNull(result);
+		Assert.assertEquals(0, result.length);
+	}
 	
+	@Test
 	public void testGetXPathByPrefix() throws Exception {
 
-		
+		String[] result = null;
+
+		result = XPathCentral.getXPathByPrefix(TestXPathPrefixEnum.XPATH_TEST);		
+		Assert.assertNotNull(result);
+		Assert.assertArrayEquals(XPATH_TEST_PREFIX_CONTENT, result);
 	}
 }
