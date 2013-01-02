82c39f48acac07a7c0bd0b7a35d47b0195f5f0eb
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-02 20:09:58 -0200

    Mapped parameterized xpaths to enums
    OPEN - task 14: Create node structure with "visitors" 
    http://github.com/fgka/senac-auditing/issues/issue/14

---
+++ audit/src/main/java/com/operativus/senacrs/audit/properties/xpath/XPathParamKey.java
@@ -0,0 +1,8 @@
+package com.operativus.senacrs.audit.properties.xpath;
+
+import com.operativus.senacrs.audit.properties.PropertyKey;
+
+public interface XPathParamKey extends PropertyKey {
+
+	int paramAmount();
+}
