c3975937a85abc4363ce9b723a4430fcfecb833e
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-04 20:07:27 -0200

    Using newly creted exception.
    OPEN - task 15: Create edge structure with web manipulators 
    http://github.com/fgka/senac-auditing/issues/issue/15

--- audit/src/main/java/com/operativus/senacrs/audit/graph/edges/webdriver/AbstractWebDriverEdge.java
+++ audit/src/main/java/com/operativus/senacrs/audit/graph/edges/webdriver/AbstractWebDriverEdge.java
@@ -2,7 +2,9 @@ package com.operativus.senacrs.audit.graph.edges.webdriver;
 
 import org.openqa.selenium.WebDriver;
 
+import com.operativus.senacrs.audit.exceptions.RuntimeExceptionFactory;
 import com.operativus.senacrs.audit.graph.nodes.Node;
+import com.operativus.senacrs.audit.graph.nodes.webdriver.WebDriverNode;
 
 public class AbstractWebDriverEdge
 		implements WebDriverEdge {
@@ -13,6 +15,9 @@ public class AbstractWebDriverEdge
 
 		super();
 
+		if (driver == null) {
+			throw RuntimeExceptionFactory.getNullArgumentException("driver");
+		}
 		this.driver = driver;
 	}
 
@@ -24,6 +29,22 @@ public class AbstractWebDriverEdge
 
 	@Override
 	public void traverse(final Node source) {
+
+		WebDriverNode castedNode = null;
+
+		if (source == null) {
+			throw RuntimeExceptionFactory.getNullArgumentException("source");
+		}
+		if (!(source instanceof WebDriverNode)) {
+			throw new IllegalSourceNodeClassException(source);
+		}
+		castedNode = (WebDriverNode) source;
+		internTraverse(castedNode);
+	}
+
+	private void internTraverse(final WebDriverNode source) {
+
+		// TODO Auto-generated method stub
 
 	}
 }
