4afaca54212d7b411c31599a1adce2ef2f6c89e5
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-13 19:43:01 -0200

    Just formating (added @Override)
    OPEN - task 14: Create node structure with "visitors" 
    https://github.com/fgka/senac-auditing/issues/issue/14

--- audit/src/main/java/com/operativus/senacrs/audit/graph/nodes/Node.java
+++ audit/src/main/java/com/operativus/senacrs/audit/graph/nodes/Node.java
@@ -1,21 +1,22 @@
 package com.operativus.senacrs.audit.graph.nodes;
 
-
 public interface Node {
-	
+
 	String END_TO_STRING = "END";
 	String START_TO_STRING = "START";
 	Node START = new Node() {
-		
+
+		@Override
 		public String toString() {
-			
+
 			return START_TO_STRING;
 		};
 	};
 	Node END = new Node() {
-		
+
+		@Override
 		public String toString() {
-			
+
 			return END_TO_STRING;
 		};
 	};
