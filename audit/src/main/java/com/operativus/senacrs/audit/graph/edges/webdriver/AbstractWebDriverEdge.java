1ef6722edfac44cd6cca760f222b8a97f5542f20
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2013-01-13 19:02:58 -0200

    checking source type before forwarding traverse() call.
    OPEN - task 14: Create node structure with "visitors" 
    https://github.com/fgka/senac-auditing/issues/issue/14

--- audit/src/main/java/com/operativus/senacrs/audit/graph/edges/webdriver/AbstractWebDriverEdge.java
+++ audit/src/main/java/com/operativus/senacrs/audit/graph/edges/webdriver/AbstractWebDriverEdge.java
@@ -1,17 +1,22 @@
 package com.operativus.senacrs.audit.graph.edges.webdriver;
 
+import java.util.Arrays;
+
 import org.openqa.selenium.WebDriver;
 
 import com.operativus.senacrs.audit.exceptions.RuntimeExceptionFactory;
 import com.operativus.senacrs.audit.graph.nodes.Node;
+import com.operativus.senacrs.audit.graph.nodes.webdriver.IllegalNodeTypeException;
 import com.operativus.senacrs.audit.graph.nodes.webdriver.WebDriverNode;
+import com.operativus.senacrs.audit.graph.nodes.webdriver.WebDriverNodeType;
 
 public abstract class AbstractWebDriverEdge
 		implements WebDriverEdge {
 
 	private final WebDriver driver;
+	private final WebDriverNodeType[] acceptedSources;
 
-	protected AbstractWebDriverEdge(final WebDriver driver) {
+	protected AbstractWebDriverEdge(final WebDriver driver, final WebDriverNodeType... acceptedSourceNodes) {
 
 		super();
 
@@ -19,6 +24,11 @@ public abstract class AbstractWebDriverEdge
 			throw RuntimeExceptionFactory.getNullArgumentException("driver");
 		}
 		this.driver = driver;
+		if (acceptedSourceNodes != null) {
+			this.acceptedSources = Arrays.copyOf(acceptedSourceNodes, acceptedSourceNodes.length);
+		} else {
+			this.acceptedSources = new WebDriverNodeType[0];
+		}
 	}
 
 	@Override
@@ -39,7 +49,25 @@ public abstract class AbstractWebDriverEdge
 			throw new IllegalSourceNodeClassException(source);
 		}
 		castedNode = (WebDriverNode) source;
-		internTraverse(castedNode);
+		this.checkSourceType(castedNode);
+		this.internTraverse(castedNode);
+	}
+
+	private void checkSourceType(final WebDriverNode source) {
+
+		boolean found = false;
+		WebDriverNodeType type = null;
+
+		type = source.getType();
+		for (WebDriverNodeType t : this.acceptedSources) {
+			if (t.equals(type)) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			throw new IllegalNodeTypeException(type);
+		}
 	}
 
 	protected abstract void internTraverse(final WebDriverNode source);
